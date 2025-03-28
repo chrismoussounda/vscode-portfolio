556bc066213df2566ff25fa67c0a79d94fe90a5d
Christoffert MOUSSOUNDA <christmoussounda@gmail.com>
2025-03-28 08:56:53 +0100

    feat: add Vercel Analytics for improved tracking

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import { useEffect } from 'react';
 import type { AppProps } from 'next/app';
+import { Analytics } from '@vercel/analytics/next';
 
 import Layout from '@/components/Layout';
 import Head from '@/components/Head';
@@ -19,6 +20,7 @@ function MyApp({ Component, pageProps }: AppProps) {
     <Layout>
       <Head title={`Christoffert MOUSSOUNDA  | ${pageProps.title}`} />
       <Component {...pageProps} />
+      <Analytics />
     </Layout>
   );
 }
